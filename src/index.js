e9f8a0f6c6402f40a07e0eef2e676e38d56eab5d
solee0524 <iblee0524@gmail.com>
2015-12-16 17:12:19 +0800

    add createModel function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
  * @param {Type}
  * @return {Type}
  */
+'use strict';
 var fs = require('fs');
 var path = require('path');
 var Promise = require('bluebird');
+var uuid = require('node-uuid');
+var Sequelize = require('sequelize');
+var moment = require('moment');
+
 var readOnlyModels = [];
 var defaultModelPath = path.resolve(process.env.PWD, 'models');
 
@@ -26,11 +31,10 @@ var getModelPath = function (modelPath, requirePaths) {
 };
 
 
-exports.init = function(modelPath) {
+exports.init = function(modelPath, ignoreModels) {
   modelPath = modelPath || defaultModelPath;
-  //console.log(modelPath);
-
-  var requirePaths = [];
+  readOnlyModels = ignoreModels || [];
+  var requirePaths = [] ;
   return getModelPath(modelPath, requirePaths).then(function () {
     return Promise.resolve(requirePaths);
   }).map(function (requirePath) {
@@ -39,5 +43,70 @@ exports.init = function(modelPath) {
     return Promise.resolve();
   });
 
+};
+
+exports.createModel = function (conn, table, schema, notAdd, validation){
+  if (typeof notAdd === 'function') {
+    validation = notAdd;
+    notAdd = '';
+  }
+
+  if (!notAdd) {
+    schema.created_at = Sequelize.INTEGER;
+    schema.updated_at = Sequelize.INTEGER;
+  }
+
+  var Model = conn.define(table, schema, {
+    timestamps: false,
+    tableName: table,
+    hooks: {
+      beforeCreate: function (item, options, fn) {
+          if (!item.id && (schema.id && !schema.id.Model.rawAttributes.id.autoIncrement))
+              item.id = uuid.v4();
+          if (typeof item.updated_at != "object" && typeof item.created_at != "object"){
+              item.created_at = moment().format('X');
+              item.updated_at = moment().format('X');
+          }
+          fn(null, item)
+      },
+      beforeUpdate: function (item, options, fn) {
+          if (typeof item.updated_at != "object"){
+              item.updated_at = moment().format('X');
+          }
+          fn(null, item)
+      },
+      beforeValidate: function (item, options, fn) {
+          if (validation)
+              return 	validation.apply(this, arguments);
+          else
+              fn(null, item)
+
+      },
+      beforeFind: function (item, fn) {
+          if (!item.order) {
+              if (this.attributes.id.type._length == 36)
+                  item.order = 'create_time desc';
+              else
+                  item.order = 'id desc';
+          }
+          fn(null, item);
+      }
+    }
+  });
+
+  Model.page = function (data, pageSize) {
+    pageSize = data.pageSize || pageSize || 20;
+    return {
+      offset: (data.pageIndex - 1) * pageSize || 0,
+      limit: data.pageSize || pageSize,
+      where: {}
+    }
+  };
+
+  Model.findById = function (id) {
+      return Model.find({where: {id: id}})
+  };
+
+  return Model;
 };
 
--- test/index.js
+++ test/index.js
@@ -2,6 +2,6 @@ import test from "tape"
 import sequlizeModelInitial from "../src"
 
 test("sequlizeModelInitial", (t) => {
-  t.plan(1)
-  t.equal(true, sequlizeModelInitial.default(), "return true")
-})
+  t.plan(1);
+  t.equal(true, sequlizeModelInitial.default(), "return true");
+});
